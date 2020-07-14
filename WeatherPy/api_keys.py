40eb037b2d321e363b1be3393ece4892c68fc3da
Carlsbeat <infinity183@gmail.com>
2020-07-13 21:50:10 -0700

    Project updates

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d2fc622ee3b3353d2dbb73f7d8081b3d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDSTjZLxavud21_6rLICmhDCD3GZJL60m4"
